e9cc1dfd325f3f0253f6191de04bdf73dc9cf1f3
Kajooja <tuomo.oksala93@gmail.com>
2026-08-28 12:32:35 +0300

    Make splash logo full-width

--- apps/mobile/src/features/branding/KajoBrand.tsx
+++ apps/mobile/src/features/branding/KajoBrand.tsx
@@ -36,8 +36,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#000000',
   },
   splashLogo: {
-    width: '108%',
-    maxWidth: 520,
+    width: '140%',
+    maxWidth: 680,
     aspectRatio: 1,
   },
   mark: {
